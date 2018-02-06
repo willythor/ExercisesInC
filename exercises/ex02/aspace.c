bf0a89ba1c2f64ba362fce9d0e118d9464fb4185
willythor <willem.thorbecke@students.olin.edu>
2018-02-05 23:01:57 -0500

    submitting exercises

--- exercises/ex02/aspace.c
+++ exercises/ex02/aspace.c
@@ -15,12 +15,22 @@ int main ()
     int var2 = 5;
     void *p = malloc(128);
     char *s = "Literal string";
+    void *z = malloc(128);
+    void *a = malloc(16);
+    void *b = malloc(16);
+
+
 
     printf ("Address of main is %p\n", main);
     printf ("Address of var1 is %p\n", &var1);
     printf ("Address of var2 is %p\n", &var2);
     printf ("Address of p is %p\n", p);
     printf ("Address of s is %p\n", s);
+    printf ("Address of var2 is %p\n", &var2);
+    printf ("Address of a is %p\n", a);
+    printf ("Address of b is %p\n", b);
+
+
 
     return 0;
 }
--- exercises/ex02/stack.c
+++ exercises/ex02/stack.c
@@ -12,21 +12,31 @@ License: GNU GPLv3
 
 int *foo() {
     int i;
+
+    // memory is allocated in the stack for an array of size 5
     int array[SIZE];
 
-    //printf("%p\n", array);
+    printf("%p\n", array);
 
     for (i=0; i<SIZE; i++) {
         array[i] = 42;
     }
+
+    // after the array is created and populated, the pointer to that 
+    // array, located in the stack, is returned   
     return array;
 }
 
 void bar() {
     int i;
+
+    //similar to above, memory for an array of size 5 is allocated
+    //in the stack, this will be placed in the same location as the
+    //array above, because local variables within functions only use
+    //temporary stack space
     int array[SIZE];
 
-    //printf("%p\n", array);
+    printf("%p\n", array);
 
     for (i=0; i<SIZE; i++) {
         array[i] = i;
@@ -39,6 +49,10 @@ int main()
     int *array = foo();
     bar();
 
+    //attempting to print contents of the foo array,
+    //but such memory was allocated temporarily for
+    //local variables on the stack within the foo()
+    //and bar() functions
     for (i=0; i<SIZE; i++) {
         printf("%d\n", array[i]);
     }
